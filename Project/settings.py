7a4de558d74c6e3e6de7f98caee8adfc60e0574b
YanaBelia <ivanna.belia138@gmail.com>
2022-05-06 23:09:03 +0300

    settings fix

--- Project/settings.py
+++ Project/settings.py
@@ -135,9 +135,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
 ]
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = '/home/IvannaBelia/Django-project/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = '/home/IvannaBelia/Django-project/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/home/IvannaBelia/Django-project/media/'
-#MEDIA_ROOT = r"C:\Users\Public.DESKTOP-T5DRGJP\Desktop\My_code\Project\static\images"
+#MEDIA_ROOT = '/home/IvannaBelia/Django-project/media/'
+MEDIA_ROOT = r"C:\Users\Public.DESKTOP-T5DRGJP\Desktop\My_code\Project\static\images"
