e71b1b0cac199b1caa54989b784e9ad8e42950f7
YanaBelia <ivanna.belia138@gmail.com>
2022-05-06 17:22:07 +0300

    settings fix

--- Project/settings.py
+++ Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+zbjqhvs)cc-wlme(d#!e^h&9pyjpw(z4r5u8-^ctor-bfj%h0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ivannabelia.pythonanywhere.com']
+ALLOWED_HOSTS = ['ivannabelia.pythonanywhere.com','198.211.99.20', 'localhost', '127.0.0.1']
 
 
 # Application definition
